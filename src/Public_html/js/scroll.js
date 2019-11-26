1fed1c946470502773a426be9e77e6caffcd7938
Ravindu <rchathuranga417@gmail.com>
2019-11-26 20:47:34 +0530

    Initial commit

---
+++ src/Public_html/js/scroll.js
@@ -0,0 +1,56 @@
+
+$(window).on('load',function() {
+    setTimeout(function () {
+        $('#loading').css('display','none');
+    },3000)
+
+});
+
+$(document).ready(function() {
+    $("#liHome").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#main").offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $("#liAbout").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#About").offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $("#liSkill").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#Skills").offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $("#liAssignment").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#Assignments").offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $("#liProject").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#Project").offset().top
+        }, 1500);
+    });
+});
+
+
+$(document).ready(function() {
+    $("#liContact").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#Contact").offset().top
+        }, 1500);
+    });
+});
